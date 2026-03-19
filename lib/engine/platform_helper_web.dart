fc90ad172b14567a00d8931fa272cdb3da6e84fa
Henderson Hood <hendy@hendersonhood.com>
2026-03-19 11:44:12 -0400

    fix: robust update mechanism with JS bridge and version bump

--- lib/engine/platform_helper_web.dart
+++ lib/engine/platform_helper_web.dart
@@ -1,25 +1,12 @@
 import 'dart:html' as html;
+import 'dart:js' as js;
 import 'platform_helper.dart';
 
 class WebPlatformHelper implements PlatformHelper {
   @override
   void reloadPage() {
-    // Clear all named caches before reloading
-    html.window.caches?.keys().then((keys) {
-      if (keys != null) {
-        for (final key in keys) {
-          html.window.caches?.delete(key);
-        }
-      }
-    }).then((_) {
-      // Force hard reload by appending timestamp
-      final uri = Uri.parse(html.window.location.href);
-      final newUri = uri.replace(queryParameters: {
-        ...uri.queryParameters,
-        'f': DateTime.now().millisecondsSinceEpoch.toString(),
-      });
-      html.window.location.replace(newUri.toString());
-    });
+    // Call the robust JS utility defined in index.html
+    js.context.callMethod('forceAppUpdate');
   }
 }
 
